8ab7d8d593cb5244b77efcb3d047847882519de1
Michael E Uhl <michael.uhl@gmail.com>
2022-12-09 13:46:31 -0500

    Problem 08
    - Python solution for both halves of problem 8, probably not super
      efficient.

--- 08/problem_08.py
+++ 08/problem_08.py
@@ -22,3 +22,34 @@ res = np.full(data.shape, True, dtype=bool)
 res[1:-1,1:-1] = np.logical_not(viz)
 print(res)
 print(np.count_nonzero(res))
+
+# end part 1
+
+llen = np.zeros(viz.shape, dtype=int)
+rlen = np.zeros_like(llen)
+tlen = np.zeros_like(llen)
+blen = np.zeros_like(llen)
+
+ss = data[1:-1,1:]
+rss = data[1:-1,:-1][:,::-1]
+for i in range(viz.shape[1]):
+    cansee = ss[:, i, None] <= ss[:,i+1:]
+    cansee[:, -1] = True
+    llen[:,i] = np.argmax(cansee, axis=1) + 1
+    cansee = rss[:, i, None] <= rss[:,i+1:]
+    cansee[:, -1] = True
+    rlen[:,i] = np.argmax(cansee, axis=1) + 1
+rlen = rlen[:,::-1]
+
+ss = data[1:,1:-1]
+rss = data[:-1,1:-1][::-1]
+for j in range(viz.shape[0]):
+    cansee = ss[None, j, :] <= ss[j+1:, :]
+    cansee[-1, :] = True
+    tlen[j,:] = np.argmax(cansee, axis=0) + 1
+    cansee = rss[None, j, :] <= rss[j+1:, :]
+    cansee[-1, :] = True
+    blen[j,:] = np.argmax(cansee, axis=0) + 1
+blen = blen[::-1]
+sscore = llen * rlen * tlen * blen
+print(sscore.max())
