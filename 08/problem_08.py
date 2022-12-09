219538c4933519bd8d51e0366b0480e89f812ea1
Michael E Uhl <michael.uhl@gmail.com>
2022-12-09 08:59:16 -0500

    Problem 08
    - Refactor part 1 solution in prep for part 2

--- 08/problem_08.py
+++ 08/problem_08.py
@@ -11,14 +11,12 @@ with open(sys.argv[1], "rt") as input_file:
         raw_data.append([int(i) for i in line.strip()])
     data = np.asarray(raw_data, dtype=int)
 
-left = data[1:-1,1:-1] <= np.maximum.accumulate(data[1:-1,:-2], axis=1)
-datav = data[:,::-1]
-right = (datav[1:-1,1:-1] <= np.maximum.accumulate(datav[1:-1,:-2], axis=1))[:,::-1]
-top = data[1:-1,1:-1] <= np.maximum.accumulate(data[:-2,1:-1], axis=0)
-datav = data[::-1,:]
-bot = (datav[1:-1,1:-1] <= np.maximum.accumulate(datav[:-2,1:-1], axis=0))[::-1,:]
-
-viz = (left & right & top & bot)
+l_max = np.maximum.accumulate(data[1:-1,:-2], axis=1)
+r_max = np.maximum.accumulate(data[:,::-1][1:-1,:-2], axis=1)[:,::-1]
+t_max = np.maximum.accumulate(data[:-2,1:-1], axis=0)
+b_max = np.maximum.accumulate(data[::-1,:][:-2,1:-1], axis=0)[::-1,:]
+ss = data[1:-1,1:-1]
+viz = (ss <= l_max) & (ss <= r_max) & (ss <= t_max) & (ss <= b_max)
 
 res = np.full(data.shape, True, dtype=bool)
 res[1:-1,1:-1] = np.logical_not(viz)
