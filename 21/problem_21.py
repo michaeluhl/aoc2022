ebb5518399bafda24e90312690512adac9d7b32b
Michael E Uhl <michael.uhl@gmail.com>
2022-12-22 22:18:19 -0500

    Problem 21
    - Python solution for part 2

--- 21/problem_21.py
+++ 21/problem_21.py
@@ -10,6 +10,35 @@ ops = {
     '=': operator.eq
 }
 
+def val_unkwn(vars, arg1, arg2):
+    try:
+        val = resolve(vars, arg1)
+        u = arg2
+    except:
+        u = arg1
+        val = resolve(vars, arg2)
+    return val, u
+
+
+def solve(vars, value, name):
+    try:
+        op, arg1, arg2 = vars[name]
+        v, u = val_unkwn(vars, arg1, arg2)
+        if op == '+':
+            return solve(vars, value -v, u)
+        elif op == '*':
+            return solve(vars, value//v, u)
+        elif op == '-' and u == arg2:
+            return solve(vars, v - value, u)
+        elif op == '-':
+            return solve(vars, value+v, u)
+        elif op == '/' and u == arg2:
+            return solve(vars, v//value, u)
+        elif op == '/':
+            return solve(vars, value*v, u)
+    except TypeError:
+        return value
+
 
 def resolve(vars, name):
     expr = vars[name]
@@ -33,3 +62,8 @@ with open(sys.argv[1], 'rt') as input_file:
             variable[name] = (tokens[1], tokens[0], tokens[2])
 
 print(resolve(variable, 'root'))
+
+variable['humn'] = None
+op, arg1, arg2 = variable['root']
+v, u = val_unkwn(variable, arg1, arg2)
+print(solve(variable, v, u))
